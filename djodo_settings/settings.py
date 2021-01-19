b4654b8522dc47061963985f8bf170e571b04acb
TANMAY007100 <tanmay_virkar@outlook.com>
2021-01-20 00:17:03 +0530

    [ADDED] Whitenoise and Brotli compression - Added Whitenoise for serving static files in Production. - Added Brotli compression to files for browsers (if supported) will be used.

--- djodo_settings/settings.py
+++ djodo_settings/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'assets'
